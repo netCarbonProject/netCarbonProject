23714843bdc8f284c3a3286f7c0d21165be1a05a
qdw77 <qkrdsmw683@gmail.com>
2025-04-14 14:46:35 +0900

    chart label correction

--- frontend/src/pages/InfoPage.jsx
+++ frontend/src/pages/InfoPage.jsx
@@ -4,6 +4,11 @@ import IndustryUsageChart from '../components/charts/IndustryUsageChart';
 import UsageCompareChart from '../components/charts/UsageCompareChart';
 import EnergySourceChart from '../components/charts/EnergySourceChart';
 import RenewableChart from '../components/charts/RenewableChart';
+import mapImg from '../assets/InfoPage/2page_map.png'
+import chart_label1 from '../assets/InfoPage/chart_label1.png'
+import chart_label2 from '../assets/InfoPage/chart_label2.png'
+import chart_label3 from '../assets/InfoPage/chart_label3.png'
+import chart_label4 from '../assets/InfoPage/chart_label4.png'
 
 const InfoPage = () => {
   useEffect(() => {
@@ -18,13 +23,26 @@ const InfoPage = () => {
 
   return (
     <div className="info-container">
-      <div className="top-section">
-        <div className="left-map">[ 지역별 전력 사용량 지도 ]</div>
+      <div className="info-top-section">
+        <div className="left-map">
+          <div className='map-wrappers'>
+            <img src={mapImg} alt='지역별 전력 사용량 지도' className='map-label'/>
+          </div>
+          <div className='map-chart'>
+              [ 지역별 전력 사용량 지도 ]
+          </div>
+        </div>
         <div className="vertical-divider" />
         <div className="right-top">
           <div className="top-charts">
-            <div className="chart"><IndustryUsageChart /></div>
-            <div className="chart"><UsageCompareChart /></div>
+            <div className='chartA'>
+              <div className='chart_label1'> <img src={chart_label1} alt='업종별 전력 사용량'/> </div>
+              <div className="chart"><IndustryUsageChart /></div>
+            </div>
+            <div className='chartB'>
+              <div className='chart_label2'> <img src={chart_label2} alt='전력 사용량 비교'/> </div>
+              <div className="chart"><UsageCompareChart /></div>
+            </div>
           </div>
           
           
@@ -32,8 +50,14 @@ const InfoPage = () => {
 
 
           <div className="bottom-charts">
-            <div className="chart"><EnergySourceChart /></div>
-            <div className="chart"><RenewableChart /></div>
+            <div className='chartC'>
+              <div className='chart_label3'> <img src={chart_label3} alt='국내 에너지원별 발전 비율'/> </div>
+              <div className="chart"><EnergySourceChart /></div>
+            </div>
+            <div className='chartD'>
+              <div className='chart_label4'> <img src={chart_label4} alt='신재생 에너지 사용 비율'/> </div>
+              <div className="chart"><RenewableChart /></div>
+            </div>
           </div>
         </div>
       </div>
